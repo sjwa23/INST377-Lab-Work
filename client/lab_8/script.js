1d94c13d57a2a7736932d8c57982d4650e838dc7
Sangbeum J <112726508+sjwa23@users.noreply.github.com>
2022-11-16 11:02:31 -0500

    Updated Lab 8

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -83,6 +83,33 @@ function filterList(array, filterInputValue) {
   });
 }
 
+function initMap() {
+  console.log('initMap');
+  const map = L.map('map').setView([38.9897, -76.9378], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  return map;
+}
+
+function markerPlace(array, map) {
+  console.log('markerPlace', array);
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
+  array.forEach((item, index) => {
+    const {coordinates} = item.geocoded_column_1;
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    if (index === 0) {
+      map.setView([coordinates[1], coordinates[0]], 10);
+    }
+  });
+}
+
 async function mainEvent() {
   /*
       ## Main Event
@@ -91,6 +118,7 @@ async function mainEvent() {
         If you separate your work, when one piece is complete, you can save it and trust it
     */
 
+  const pageMap = initMap();
   // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
   const submit = document.querySelector('#get-resto'); // get a reference to your submit button
@@ -135,6 +163,7 @@ async function mainEvent() {
     console.log('input', event.target.value);
     const newFilterList = filterList(currentList, event.target.value);
     injectHTML(newFilterList);
+    markerPlace(newFilterList, pageMap);
   });
 
   // And here's an eventListener! It's listening for a "submit" button specifically being clicked
@@ -149,6 +178,7 @@ async function mainEvent() {
 
     // And this function call will perform the "side effect" of injecting the HTML list for you
     injectHTML(currentList);
+    markerPlace(currentList, pageMap);
 
     // By separating the functions, we open the possibility of regenerating the list
     // without having to retrieve fresh data every time
